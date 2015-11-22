7b0a707216e9fb0000c15ac6cbd15963e351b8fe
Noel Rappin <noel@tablexi.com>
2015-11-22 16:29:38 -0600

    width

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,9 @@ class OrdersController < ApplicationController
 
   def create
     token = StripeToken.new(**card_params)
-    action = PurchasesCart.new(user: current_user, stripe_token: token,
-                               purchase_amount_cents: params[:purchase_amount_cents])
+    action = PurchasesCart.new(
+      user: current_user, stripe_token: token,
+      purchase_amount_cents: params[:purchase_amount_cents])
     action.run
     if action.success
       redirect_to order_path(id: action.order.reference)
@@ -18,7 +19,8 @@ class OrdersController < ApplicationController
 
   private def card_params
     params.slice(
-      :credit_card_number, :expiration_month, :expiration_year, :cvc).symbolize_keys
+      :credit_card_number, :expiration_month,
+      :expiration_year, :cvc).symbolize_keys
   end
 
 end
