1a74b8aa1efa5b1d7623d7f0a10af69ffce642f5
Noel Rappin <noel@tablexi.com>
2015-12-27 20:57:51 -0600

    switch to active job

--- app/actions/purchases_cart.rb
+++ app/actions/purchases_cart.rb
@@ -1,17 +1,17 @@
-## START: purchases_cart_init
 class PurchasesCart
 
   attr_accessor :user, :stripe_token, :purchase_amount, :success, :order,
-                :stripe_charge, :expected_ticket_ids
+                :stripe_charge, :expected_ticket_ids, :order_reference
 
   def initialize(user:, stripe_token:, purchase_amount_cents:,
-                 expected_ticket_ids:)
+                 expected_ticket_ids:, order_reference: nil)
     @user = user
     @stripe_token = stripe_token
     @purchase_amount = Money.new(purchase_amount_cents)
     @success = false
     @continue = true
     @expected_ticket_ids = expected_ticket_ids.split(" ").map(&:to_i).sort
+    @order_reference = order_reference || Order.generate_reference
   end
 
   def run
@@ -20,9 +20,7 @@ class PurchasesCart
     post_charge
     @success = @continue
   end
-  ## END: purchases_cart_init
 
-  ## START: purchases_pre_charge
   def pre_charge_valid?
     purchase_amount == tickets.map(&:price).sum &&
       expected_ticket_ids == tickets.map(&:id).sort
@@ -49,7 +47,7 @@ class PurchasesCart
   def create_order
     self.order = Order.new(
       user_id: user.id, price_cents: purchase_amount.cents, status: "created",
-      reference: Order.generate_reference, payment_method: "stripe")
+      reference: order_reference, payment_method: "stripe")
     tickets.each do |ticket|
       order.order_line_items.build(
         ticket_id: ticket.id, price_cents: ticket.price.cents)
@@ -61,9 +59,7 @@ class PurchasesCart
       order.save
     end
   end
-  ## END: purchases_pre_charge
 
-  ## START: purchases_charge
   def charge
     return unless @continue
     @stripe_charge = StripeCharge.new(token: stripe_token, order: order)
@@ -81,13 +77,10 @@ class PurchasesCart
     save
     @continue = false
   end
-  ## END: purchases_charge
 
-  ## START: purchases_post_charge
   def post_charge
     return unless @continue
     @continue = save && order.succeeded?
   end
-  ## END: purchases_post_charge
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,24 +5,10 @@ class OrdersController < ApplicationController
   end
 
   def create
-    token = StripeToken.new(**card_params)
-    action = PurchasesCart.new(
-      user: current_user, stripe_token: token,
-      purchase_amount_cents: params[:purchase_amount_cents],
-      expected_ticket_ids: params[:ticket_ids])
-    action.run
-    if action.success
-      redirect_to order_path(id: action.order.reference)
-    else
-      redirect_to shopping_cart_path
-    end
+    reference = Order.generate_reference
+    PurchasesCartJob.perform_later(
+      user: current_user, params: params, order_reference: reference)
+    redirect_to order_path(id: reference)
   end
-
-  # START: card_params
-  private def card_params
-    params.slice(:credit_card_number, :expiration_month,
-                 :expiration_year, :cvc, :stripe_token).symbolize_keys
-  end
-  # END: card_params
 
 end
