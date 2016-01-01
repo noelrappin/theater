3b26203a3dc59e72e182557cc2d9e00116664f85
Noel Rappin <noel@tablexi.com>
2016-01-01 17:08:42 -0600

    oops

--- app/jobs/purchases_cart_setup_job.rb
+++ app/jobs/purchases_cart_setup_job.rb
@@ -1,10 +1,19 @@
-class PurchasesCartChargeJob < ActiveJob::Base
+class PurchasesCartSetupJob < ActiveJob::Base
 
   queue_as :default
 
-  def perform(order)
-    charge_action = PurchasesCartCharge.new(order)
-    charge_action.run
+  def perform(user:, params:, order_reference:)
+    token = StripeToken.new(**card_params(params))
+    purchases_cart_action = PurchasesCart.new(
+      user: user, stripe_token: token, purchase_amount_cents:
+      params[:purchase_amount_cents], expected_ticket_ids: params[:ticket_ids],
+      order_reference: order_reference)
+    purchases_cart_action.run
+  end
+
+  private def card_params(params)
+    params.slice(:credit_card_number, :expiration_month,
+                 :expiration_year, :cvc, :stripe_token).symbolize_keys
   end
 
 end
