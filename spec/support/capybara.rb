08bacf192ce876f5a05762a6f48137b38697829f
Noel Rappin <noel@tablexi.com>
2016-03-02 21:12:27 -0600

    cop

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,4 @@
 require "capybara/poltergeist"
 require "capybara-screenshot/rspec"
 Capybara.asset_host = "http://localhost:3000"
-options = {js_errors: false}
 Capybara.javascript_driver = :poltergeist_billy
