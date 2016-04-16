041df054dc3e361cf1e3316dae0bf8cea8b705e1
Noel Rappin <noel@tablexi.com>
2016-04-16 08:53:30 -0500

    labels

--- app/workflows/creates_plan.rb
+++ app/workflows/creates_plan.rb
@@ -13,10 +13,39 @@ class CreatesPlan
     @ticket_category = ticket_category
   end
 
+  def remote_stripe_plan(remote_id)
+    Stripe::Plan.retrieve(remote_id)
+  end
+
   def run
-    remote_plan = Stripe::Plan.create(
-      id: remote_id, amount: price_cents,
-      currency: "usd", interval: interval, name: plan_name)
+    unless remote_stripe_plan
+      Stripe::Plan.create(
+        id: remote_id, amount: price_cents,
+        currency: "usd", interval: interval, name: plan_name)
+    end
+    api = PayPal::SDK::REST::API.new
+    paypal_remote_plan = PayPal::SDK::REST::Plan.new(name: plan_name,
+      type: "fixed",
+      token: api.token,
+      description: plan_name,
+      payment_definitions: [{
+        name: "Regular Payments",
+        type: "REGULAR",
+        frequency_interval: "1",
+        frequency: interval.upcase,
+        cycles: 12,
+        amount: {value: price_cents / 100, currency: "USD"},
+        }],
+      merchant_preferences: {
+        cancel_url: "http://host/paypal/cancel",
+        return_url: "http://host/paypal/return"
+        })
+    result = paypal_remote_plan.create
+    patch = PayPal::SDK::REST::Patch.new
+    patch.op = "replace"
+    patch.path = "/"
+    patch.value = {state: "ACTIVE"}
+    update = paypal_remote_plan.update(patch)
     self.plan = Plan.create(
       remote_id: remote_plan.id, plan_name: plan_name,
       price_cents: price_cents, interval: interval,
--- lib/tasks/plan_creation.rake
+++ lib/tasks/plan_creation.rake
@@ -1,7 +1,6 @@
 namespace :theater do
 
-  task create_plans: :environment do
-    plans = [
+  PLANS = [
       {remote_id: "orchestra_monthly", plan_name: "Orchestra Monthly",
        price_cents: 10_000, interval: "month", tickets_allowed: 1,
        ticket_category: "Orchestra"},
@@ -12,8 +11,10 @@ namespace :theater do
        price_cents: 30_000, interval: "month", tickets_allowed: 1,
        ticket_category: "VIP"}
     ]
+
+  task create_plans: :environment do
     Plan.transaction do
-      plans.each { |plan_data| CreatesPlan.new(**plan_data).run }
+      PLANS.each { |plan_data| CreatesPlan.new(**plan_data).run }
     end
   end
 
