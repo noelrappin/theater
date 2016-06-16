1b1c24f18a1c47c08754922248e10d7d925a3f2b
Noel Rappin <noel@tablexi.com>
2016-06-15 20:35:28 -0500

    cash in controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,13 +5,14 @@ class PaymentsController < ApplicationController
     @payment = Payment.find_by(reference: @reference)
   end
 
-  # START: with_discount
+  # START: cash_workflow
   def create
     if params[:discount_code].present?
       session[:new_discount_code] = params[:discount_code]
       redirect_to shopping_cart_path
       return
     end
+    normalize_purchase_amount
     workflow = create_workflow(params[:payment_type], params[:purchase_type])
     workflow.run
     if workflow.success
@@ -21,7 +22,6 @@ class PaymentsController < ApplicationController
       redirect_to shopping_cart_path
     end
   end
-  # END: with_discount
 
   private def failure_path
     shopping_cart_path
@@ -29,12 +29,35 @@ class PaymentsController < ApplicationController
 
   private def create_workflow(payment_type, purchase_type)
     case purchase_type
-    when "SubscriptionCart"
-      stripe_subscription_workflow
-    when "ShoppingCart"
-      (payment_type == "paypal") ? paypal_workflow : stripe_workflow
+    when "SubscriptionCart" then stripe_subscription_workflow
+    when "ShoppingCart" then payment_workflow(payment_type)
     end
   end
+
+  private def payment_workflow(payment_type)
+    case payment_type
+    when "paypal" then paypal_workflow
+    when "stripe" then stripe_workflow
+    when "cash" then cash_workflow
+    when "invoice" then cash_workflow
+    end
+  end
+
+  private def cash_workflow
+    CashPurchasesCart.new(
+      user: current_user,
+      purchase_amount_cents: params[:purchase_amount_cents],
+      expected_ticket_ids: params[:ticket_ids],
+      discount_code: session[:new_discount_code])
+  end
+
+  private def normalize_purchase_amount
+    return if params[:purchase_amount].blank?
+    params[:purchase_amount_cents] =
+      (params[:purchase_amount].to_f * 100).to_i
+  end
+
+  # START: cash_workflow
 
   private def stripe_subscription_workflow
     StripeCreatesSubscription.new(
@@ -43,7 +66,6 @@ class PaymentsController < ApplicationController
       token: StripeToken.new(**card_params))
   end
 
-  # START: controller_with_code
   private def paypal_workflow
     PayPalPurchasesCart.new(
       user: current_user,
@@ -59,7 +81,6 @@ class PaymentsController < ApplicationController
       payment_reference: reference,
       discount_code: session[:new_discount_code])
   end
-  # END: controller_with_code
 
   private def card_params
     params.slice(:credit_card_number, :expiration_month,
